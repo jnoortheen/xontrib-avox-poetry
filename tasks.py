389d2647b4ae4fae3ef252755c00be1f3dc42216
Noortheen Raja <jnoortheen@gmail.com>
2020-12-17 03:39:52 +0530

    feat: init from cookiecutter

---
+++ tasks.py
@@ -0,0 +1,71 @@
+import typing as tp
+import subprocess as sp
+from arger import Arger, Argument
+from colorama import Fore, Style
+
+arger = Arger(
+    description="Common set of tasks to run",
+)
+
+
+def run(*args, **kwargs):
+    cmd = " ".join(args)
+    print(Fore.GREEN + cmd + Style.RESET_ALL)
+    out = sp.check_output(args, **kwargs)
+
+    return out
+
+
+@arger.add_cmd
+def release(
+    type: tp.cast(
+        str,
+        Argument(
+            choices=[
+                "patch",
+                "minor",
+                "major",
+                "prepatch",
+                "preminor",
+                "premajor",
+                "prerelease",
+            ],
+        ),
+    ) = "patch"
+):
+    """Bump version, tag and push them.
+
+    Args:
+        type: version bump as supported by `poetry version` command
+    """
+    run(["poetry", "version", type])
+    _, version = run("poetry", "version").split()
+
+    version_num = f"v{version}"
+    msg = f"chore: bump version to {version_num}"
+
+
+    # if you want to keep changelog install packages like
+    # - https://pypi.org/project/git-changelog/
+    # 
+    # # tagging the current commit to place the name correct in the changelog
+    # run(f"git tag {version_num}")
+    # run("git-changelog", ".", "-s", "angular", "-o", "CHANGELOG.md")
+    # run("git status")
+
+    answer = input(f"{msg}\nAdd to commit: [Y/n]?")
+    if answer.lower() in {"no", "n"}:
+        return
+    run("git add .")
+    run(f'git commit -m "{msg}"')
+
+    # using force to move the tag to the latest commit.
+    run(f"git tag {version_num} --force")
+
+    run("git push")
+    run("git push --tags")
+
+
+
+if __name__ == "__main__":
+    arger.run()
---
+++ xontrib/avox-poetry.xsh
@@ -0,0 +1,8 @@
+import xonsh
+
+
+__all__ = ()
+
+print('This is avox-poetry!')
+
+# Good start! Get more documentation -> https://xon.sh/contents.html#guides
